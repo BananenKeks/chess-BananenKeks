2af6b8b22a9da46bfa97f7582e782584d37aba90
BananenKeks <jamie.gawehn@gmail.com>
2017-03-20 17:51:37 +0100

    Update class Main
    Added comments.
    Added more structure.
    Removed junk.

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
-
+//Main class.
 public class Main {
-
 	public static void main(String[] args) {
-		 
+		//Create objetct f from class Figure.
+		//TODO connect classes.
+		@SuppressWarnings("unused")
 		Figure f = new Figure();
-		//@SuppressWarnings("unused")
+		//Create object start from class Board.
+		@SuppressWarnings("unused")
 		Board start = new Board();
+		//END of object creation.
 	}
-
 }
+//END Main class.
