788733b418c7f3f3b961baf0ad04154d0d517e01
BananenKeks <jamie.gawehn@gmail.com>
2017-04-25 11:47:10 +0200

    Update Main.java
    Added array for debuging purpose

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
+import java.io.IOException;
+
 //Main class.
 public class Main {
-	public static void main(String[] args) {
-		// Create objetct f from class Figure.
-		// TODO connect classes.
-		// @SuppressWarnings("unused")
-		// Pawn f = new Pawn(5, 1, 2);
-		// f.calculateAllowedMovement();
-		// Create object start from class Board.
-		// @SuppressWarnings("unused")
-		Board start = new Board();
-		
-
-		// END of object creation.
+	public static Figure click = new Figure();
+	public static Board start = new Board();
+	private static int aMoving[][] = new int[8][8];
+	public static void main(String[] args) throws IOException {
+		// Figure F = new Figure();
+	}
+	public static int[][] getaMoving() {
+		return aMoving;
+	}
+	public static void setaMoving(int aMoving[][]) {
+		Main.aMoving = aMoving;
 	}
 }
 // END Main class.
